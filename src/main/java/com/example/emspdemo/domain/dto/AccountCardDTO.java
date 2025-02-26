f0a29d07f97b967a3a0af73820fbe77b2e39eb2d
sunhe <sunhe_2004@hotmail.com>
2025-02-26 10:44:38 +0800

    add Serializable

--- src/main/java/com/example/emspdemo/domain/dto/AccountCardDTO.java
+++ src/main/java/com/example/emspdemo/domain/dto/AccountCardDTO.java
@@ -1,10 +1,15 @@
 package com.example.emspdemo.domain.dto;
 
 import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.List;
 
 @Data
-public class AccountCardDTO {
+public class AccountCardDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 3152164729784306846L;
     private AccountDTO account;
     private List<CardDTO> cards;
 }
